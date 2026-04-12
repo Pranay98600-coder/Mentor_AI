753ac697cdfc55ee14dd758b6bb26f9954ce79c6
Pranay <salunkhepranay14@gmail.com>
2026-04-12 17:28:54 +0530

    hide pass using json ignore properties

--- mentorai-backend/src/main/java/com/mentorai/model/User.java
+++ mentorai-backend/src/main/java/com/mentorai/model/User.java
@@ -2,9 +2,10 @@ package com.mentorai.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDateTime;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-
+@JsonIgnoreProperties({"password"})
 @Entity
 @Table(name = "users")
 public class User {
